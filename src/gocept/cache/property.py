2a4c8e44de93bf8761c6029dbb112986bf02bb88
Christian Zagrodnick <cz@flyingcircus.io>
2007-09-18 12:12:52 +0000

    added transaction bound cache

---
+++ src/gocept/cache/property.py
@@ -0,0 +1,30 @@
+# Copyright (c) 2007 gocept gmbh & co. kg
+# See also LICENSE.txt
+# $Id$
+
+import transaction
+
+
+class TransactionBoundCache(object):
+
+    def __init__(self, name, factory):
+        self.attribute = name
+        self.factory = factory
+
+    def __get__(self, instance, class_):
+        try:
+            cache = getattr(instance, self.attribute)
+        except AttributeError:
+            cache = self.factory()
+            setattr(instance, self.attribute, cache)
+            transaction.get().addBeforeCommitHook(
+                self.invalidate, (instance, ))
+            transaction.get().addAfterCommitHook(
+                lambda commit_or_abort: self.invalidate, (instance, ))
+        return cache
+
+    def invalidate(self, instance):
+        try:
+            delattr(instance, self.attribute)
+        except AttributeError:
+            pass
