e23ae923ed6560ebd3c657b9107bff90958dbbed
Christian Zagrodnick <cz@flyingcircus.io>
2007-09-18 10:34:58 +0000

    gocept.cache package

---
+++ setup.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2007 gocept gmbh & co. kg
+# See also LICENSE.txt
+# $Id$
+
+import os.path
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name = 'gocept.cache',
+    version = "0.1",
+    author = "Christian Zagrodnick",
+    author_email = "cz@gocept.com",
+    description = "Cache descriptors for Python and Zope",
+    long_description = file(os.path.join(os.path.dirname(__file__),
+                                         'src', 'gocept', 'cache',
+                                         'README.txt')).read(),
+    license = "ZPL 2.1",
+    url='http://pypi.python.org/pypi/gocept.cache',
+
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+
+    include_package_data = True,
+    zip_safe = False,
+
+    namespace_packages = ['gocept'],
+    install_requires = [
+        'decorator',
+        'setuptools',
+        'ZODB3',
+    ],
+    extras_require = dict(
+        test=['zope.testing'])
+    )
