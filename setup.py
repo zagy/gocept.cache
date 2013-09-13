eaf3c52708ac1acd9d6ee05f163c47e6152bec54
Florian Pilz <fp@gocept.com>
2013-09-13 13:30:03 +0200

    Preparing release 0.6

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'gocept.cache',
-    version='0.6b3.dev0',
+    version='0.6',
     author = "Christian Zagrodnick",
     author_email = "cz@gocept.com",
     description = "Cache descriptors for Python and Zope",
