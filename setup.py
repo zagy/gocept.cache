8ad0de7a714c4425e13db746dbba6e15255fc521
Wolfgang Schnerring <ws@gocept.com>
2014-06-10 12:55:29 +0200

    pep8

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name = 'gocept.cache',
+    name='gocept.cache',
     version='0.6.2.dev0',
-    author = "Christian Zagrodnick",
-    author_email = "cz@gocept.com",
-    description = "Cache descriptors for Python and Zope",
+    author="Christian Zagrodnick",
+    author_email="cz@gocept.com",
+    description="Cache descriptors for Python and Zope",
     classifiers=[
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.6",
@@ -20,22 +20,22 @@ setup(
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
     ],
-    long_description = (
+    long_description=(
         open(os.path.join(os.path.dirname(__file__),
                           'src', 'gocept', 'cache', 'README.txt')).read() +
         '\n\n' +
         open(os.path.join(os.path.dirname(__file__), 'CHANGES.txt')).read()),
-    license = "ZPL 2.1",
+    license="ZPL 2.1",
     url='http://pypi.python.org/pypi/gocept.cache',
 
-    packages = find_packages('src'),
-    package_dir = {'': 'src'},
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
 
-    include_package_data = True,
-    zip_safe = False,
+    include_package_data=True,
+    zip_safe=False,
 
-    namespace_packages = ['gocept'],
-    install_requires = [
+    namespace_packages=['gocept'],
+    install_requires=[
         'decorator',
         'setuptools',
         'transaction',
