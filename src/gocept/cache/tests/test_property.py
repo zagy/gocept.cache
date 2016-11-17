375c8b253e0c3bac01ca09b6405701f873bc0c6a
Michael Howitz <mh@gocept.com>
2016-11-17 13:47:22 +0100

    Raise `TransactionJoinError` if joining the transaction failed in ``.property.TransactionBoundCache``.

--- src/gocept/cache/tests/test_property.py
+++ src/gocept/cache/tests/test_property.py
@@ -1,5 +1,6 @@
+import pytest
 import transaction
-from gocept.cache.property import TransactionBoundCache
+from gocept.cache.property import TransactionBoundCache, TransactionJoinError
 
 try:
     import unittest.mock as mock
@@ -21,8 +22,8 @@ def test_property__TransactionBoundCache__invalidate__1():
     transaction.get()._resources[0].cache.invalidate(foo)
 
 
-class VoteExceptionDataManager(object):
-    """DataManager which raises an exception in tpc_vote."""
+class NoopDatamanager(object):
+    """Datamanager which does nothing."""
 
     def abort(self, trans):
         pass
@@ -33,11 +34,15 @@ class VoteExceptionDataManager(object):
     def tpc_begin(self, trans):
         pass
 
-    def tpc_vote(self, trans):
-        raise RuntimeError()
-
     def tpc_abort(self, trans):
         pass
+
+
+class VoteExceptionDataManager(NoopDatamanager):
+    """DataManager which raises an exception in tpc_vote."""
+
+    def tpc_vote(self, trans):
+        raise RuntimeError()
 
     def sortKey(self):
         # Make sure CacheDataManager.abort() is not called by making sure we
@@ -59,3 +64,27 @@ def test_property__CacheDataManager__tpc_abort__1():
     # We cannot join a transaction in status COMMITFAILED:
     with mock.patch('transaction._transaction.Transaction.join'):
         assert {} == foo.cache
+
+
+class JoiningTransactionBoundCacheDuringCommitDataManager(NoopDatamanager):
+    """Datamanager which accesses a TransactionBoundCache during commit."""
+
+    def __init__(self, obj, cache_attr):
+        self.obj = obj
+        self.cache_attr = cache_attr
+
+    def commit(self, trans):
+        getattr(self.obj, self.cache_attr)
+
+
+def test_property__TransactionBoundCache____get____1():
+    """It raises a TransactionJoinError if called first time during commit."""
+    foo = Foo()
+    dm = JoiningTransactionBoundCacheDuringCommitDataManager(foo, 'cache')
+    txn = transaction.begin()
+    txn.join(dm)
+    with pytest.raises(TransactionJoinError) as err:
+        transaction.commit()
+    assert (
+        "expected txn status 'Active' or 'Doomed', but it's 'Committing'" ==
+        str(err.value))
