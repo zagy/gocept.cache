c65d1d4c199d3198df9c58d16e92aec1d122bcec
Steffen Allner <sa@gocept.com>
2018-11-16 11:25:12 +0100

    re #2: Bring test coverage to 100 %.

--- src/gocept/cache/tests/test_method.py
+++ src/gocept/cache/tests/test_method.py
@@ -40,3 +40,54 @@ class TestCollect(zope.testing.cleanup.CleanUp, unittest.TestCase):
         # Assert only that 'value' is there.
         self.assertEqual('value', list(
             list(gocept.cache.method._caches.values())[0].values())[0][0])
+
+    def test_method__collect__1(self):
+        """It cleans up caches which timed out."""
+        import gocept.cache.method
+        import time
+
+        @gocept.cache.method.Memoize(0.2)
+        def takes_a_little_while(arg):
+            return arg
+
+        takes_a_little_while('one')
+        time.sleep(0.1)
+        takes_a_little_while('two')
+        time.sleep(0.3)
+        takes_a_little_while('three')
+        func_cache = list(gocept.cache.method._caches.values())[0]
+        assert 3 == len(func_cache.values())
+        assert sorted([x[0] for x in func_cache.values()]) == [
+            'one', 'three', 'two']
+
+        gocept.cache.method.collect()
+        func_cache = list(gocept.cache.method._caches.values())[0]
+        assert 1 == len(func_cache.values())
+        assert 'three' == list(func_cache.values())[0][0]
+
+
+class TestMemoize(zope.testing.cleanup.CleanUp, unittest.TestCase):
+    """Testing ..method.Memoize"""
+
+    def test_method__Memoize__1(self):
+        """It applies `ignore_self` only if `self` is the first argument
+
+        in the signature."""
+        import gocept.cache.method
+
+        @gocept.cache.method.Memoize(0.1, ignore_self=True)
+        def no_self_method(first, second):
+            return (first, second)
+
+        @gocept.cache.method.Memoize(0.1, ignore_self=True)
+        def self_method(self, second):
+            return (self, second)
+
+        no_self_method('one', 'two')
+        self_method('three', 'four')
+        # We have two different functions with a different length of keys
+        cached_keys = [
+            list(x.keys())[0][0]
+            for x in gocept.cache.method._caches.values()
+        ]
+        assert [('four',), ('one', 'two')] == sorted(cached_keys)
